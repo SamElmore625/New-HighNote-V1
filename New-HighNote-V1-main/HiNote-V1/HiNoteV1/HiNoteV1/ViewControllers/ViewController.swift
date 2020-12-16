b4b040e54cf751bdd4a41444e1e4594a0b1fb32a
Dev Jijilal <35881411+devjij@users.noreply.github.com>
2020-12-15 21:30:21 -0500

    Add files via upload

---
+++ New-HighNote-V1-main/HiNote-V1/HiNoteV1/HiNoteV1/ViewControllers/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HiNoteV1
+//
+//  Created by Dev Jijilal on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var signupButton: UIButton!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
