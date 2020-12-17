a559b7b8ae7cf4d018ee8e79be90c93ad51e5c1f
SamElmore625 <54898090+SamElmore625@users.noreply.github.com>
2020-12-16 20:14:18 -0500

    bruh

---
+++ HighNote-V1-main/HiNote-V1/HiNoteV1/HiNoteV1/PleaseWorkViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PleaseWorkViewController.swift
+//  HiNoteV1
+//
+//  Created by Sam Elmore on 12/15/20.
+//
+
+import UIKit
+
+class PleaseWorkViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
